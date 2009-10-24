f27d8182fddeab9e8a42eaca8c7c94ba4f1b6a78
Jacob Atkins <jta4j@mcs.uvawise.edu>
2009-10-24 18:41:00 -0400

    finished up 06

--- scripts/06_base-finish.rb
+++ scripts/06_base-finish.rb
@@ -5,15 +5,33 @@ require 'fileutils'
 #
 # jta4j@mcs.uvawise.edu
 
-puts 'Installing syslog-ng, vixie-cron, nfs-utils, and portmap'
+puts 'Installing the required base packages'
 
-`chroot /mnt/gentoo emerge syslog-ng vixie-cron nfs-utils portmap`
+`chroot /mnt/gentoo emerge syslog-ng vixie-cron nfs-utils portmap dhcp`
 
 puts 'Done installing the tools'
+
+puts 'Updaing the world'
+
+`chroot /mnt/gentoo emerge -nDu world`
+
+puts 'Done updaing'
 
 puts 'Removing stage3 file'
 
 FileUtils.rm_f /mnt/gentoo/stage*
+
+puts 'Unmounting file systems'
+
+`umount /mnt/gentoo/dev`
+`umount /mnt/gentoo/proc`
+`umount /mnt/gentoo/usr/portage`
+`umount /mnt/gentoo/boot`
+`umount /mnt/gentoo`
+
+puts 'Rebooting'
+
+`reboot`
 
 
 
