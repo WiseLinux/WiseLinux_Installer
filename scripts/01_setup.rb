aa377bddca3518c773cd8ea93e4a04987afa6e11
jatkins <jta4j@mcs.uvawise.edu>
2009-10-31 16:27:57 -0400

    adding yaml config settings to 01_setup

--- scripts/01_setup.rb
+++ scripts/01_setup.rb
@@ -1,26 +1,29 @@
 #!/usr/bin/ruby
+require "yaml"
+
 # This script will setup the network and partion/format the harddrive
 # Jacob Atkins
 # Univerisity of Virginia's College at Wise
 #
 # jta4j@mcs.uvawise.edu
 
+CONFIG = YAML::load(File.read('../includes/config.yml'))
+
 puts 'Configuring interface eth0'
 
 file = File.open('/etc/resolv.conf', 'w')
 
-file.puts "search mcs.uvawise.edu"
-file.puts "nameserver 10.10.10.1"
-file.puts "nameserver 143.60.60.20"
-file.puts "nameserver 143.60.60.22"
+file.puts "search #{CONFIG['setup']['dns']['domain']}"
+file.puts "nameserver #{CONFIG['setup']['dns']['nameserver1']}" 
+file.puts "nameserver #{CONFIG['setup']['dns']['nameserver2']}"
 
 file.close
 
 puts 'Finished configuring eth0'
 
-puts 'Making partitions on sda'
+puts "Making partitions on #{CONFIG['setup']['harddrive']['harddrive']}"
 
-`sfdisk -q /dev/sda < /root/includes/sda.part`
+`sfdisk -q /dev/#{CONFIG['setup']['harddrive']['harddrive']} < ../includes/#{CONFIG['setup']['harddrive']['harddrive_map_file']}"`
 
 puts 'Finished making partitions'
 
